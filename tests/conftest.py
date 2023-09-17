9b668b7c487cc487a3907bb65fb11b37422f7789
dav ost <private>
2023-09-17 14:16:48 +0300

    added command + tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,39 @@
+import random
+import string
+
 import pytest
 from unittest.mock import Mock
+
+from typing import BinaryIO
+
+
+def _random_str():
+    return "".join(random.choices(string.ascii_letters + string.digits, k=random.randint(1, 10)))
+
+
+def _random_str_list():
+    return [_random_str() for _ in range(random.randint(1, 3))]
+
+
+@pytest.fixture
+def binary_io_factory():
+    def factory():
+        return Mock(spec=BinaryIO)
+
+    return factory
+
+
+@pytest.fixture
+def timeout_factory():
+    def factory():
+        return random.uniform(1, 100)
+
+    return factory
+
+
+@pytest.fixture
+def cmd_factory():
+    def factory():
+        return random.choice([_random_str(), _random_str_list()])
+
+    return factory
